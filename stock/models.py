c783c1a74555fda7bc9671c2ed55866029794752
hosstimus <coolhoss@hotmail.com>
2017-08-16 17:56:40 +1000

    Added primary key to stock

--- stock/models.py
+++ stock/models.py
@@ -2,7 +2,7 @@ from django.db import models
 # Create your models here.
 
 class Inventory(models.Model)
-    itemID = models.CharField(max_length=30, unique=True)
+    itemID = models.CharField(max_length=30, primary_key=True)
     availiableQuantity = models.IntegerField
     reservedQuantity = models.IntegerField
     orderedQuantity = models.IntegerField
