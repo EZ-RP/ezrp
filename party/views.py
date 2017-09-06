fc1395a85f7463670d964203bbb6237b7127fd54
Sam Fountain <sam.sauce@gmail.com>
2017-09-06 16:24:43 +1000

    Customer table page

--- party/views.py
+++ party/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from party.models import Party
 from django.http import HttpResponse
-# Create your views here.
 
 
-def main(request):
-    return render(request, 'party/Parties.html')
+def all_customers(request):
+    return render(request, 'party/customer/all_customers.html', {'customers': Party.objects.all()})
+
+
+def customers(request):
+    return render(request, 'party/customer/customers.html')
+
+
+def vendors(request):
+    return render(request, 'party/customer/customers.html')
+
+
+
