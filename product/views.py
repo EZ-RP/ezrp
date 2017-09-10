dc7678ee20d922e6fd928e57a0fc54eb291de1d7
Daley <benjamin.daley@uon.edu.au>
2017-09-11 02:30:05 +1000

    Preliminary Product list view. Doesn't work.

--- product/views.py
+++ product/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def main(request):
-    return render(request, 'product/Product.html')
+def product(request):
+    return render(request, 'product/product.html', {'product': product.objects.all()})
