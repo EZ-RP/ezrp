defb4c9a314375ba2ad3c135a219a756f658b732
martinoc <martin.o.94@hotmail.com>
2017-09-11 12:29:33 +1000

    HR table up and running.

--- human_resources/urls.py
+++ human_resources/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.main, name='humanResources_Main'),
+    url(r'^$', views.main, name='humanResources_Main'),
+    url(r'^all_employees/$', views.employees, name='all_employees'),
 ]
--- human_resources/views.py
+++ human_resources/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from human_resources.models import Employee
 from django.http import HttpResponse
 # Create your views here.
 
 
 def main(request):
     return render(request, 'human_resources/humanResources.html')
+
+
+def employees(request):
+    return render(request, 'human_resources/all_employees.html', {'employees': Employee.objects.all()})
