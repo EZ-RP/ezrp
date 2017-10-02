cbaa6680569cd1d5e6db977d43c75fb404f1586d
Sam Fountain <sam.sauce@gmail.com>
2017-10-02 11:20:52 +1100

    form view

--- party/views.py
+++ party/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from party.models import Party
 from django.http import HttpResponse
+from .forms import PartyForm
 
 
 def all_customers(request):
@@ -9,6 +10,18 @@ def all_customers(request):
 
 def customers(request):
     return render(request, 'party/customer/customers.html')
+
+
+def customer_new(request):
+    if request.method == "POST":
+        form = PartyForm(request.POST)
+        if form.is_valid():
+            party = form.save(commit=False)
+            party.party_type = 'C'
+            party.save()
+    else:
+        form = PartyForm()
+    return render(request, 'party/customer/customer_new.html', {'form': form})
 
 
 def vendors(request):
