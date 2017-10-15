aff2035865e81e7803fa97509aa0e777b45df01e
Daley <benjamin.daley@uon.edu.au>
2017-10-15 17:04:05 +1100

    Updated table css. Started work on a product categories page

--- product/views.py
+++ product/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from product.models import Item
 from django.http import HttpResponse
 from .forms import ProductForm
+from product.tables import ProductTable
 # Create your views here.
 
 def product_new(request):
@@ -10,10 +11,13 @@ def product_new(request):
         if form_product.is_valid():
             prod = form_product.save(commit=False)
             prod.save()
-            return render(request, 'product/product.html', {'product': Item.objects.all()})
     else:
         form_product = ProductForm()
     return render(request, 'product/product_new.html', {'form_product': form_product})
 
 def product(request):
-    return render(request, 'product/product.html', {'product': Item.objects.all()})
+    prodtable = ProductTable(Item.objects.all())
+
+    return render(request, 'product/product.html', {
+        'prodtable': prodtable
+    })
