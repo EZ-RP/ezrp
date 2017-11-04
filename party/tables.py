1002fa887a7da7ca2dc89369196b3c08c062e7a5
Sam Fountain <sam.sauce@gmail.com>
2017-11-04 22:17:47 +1100

    vendors

--- party/tables.py
+++ party/tables.py
@@ -34,3 +34,35 @@ class CustomerAddress(tables.Table):
         model = PartyAddress
         fields = ['address_id.street', 'address_id.suburb', 'address_id.state', 'address_id.postcode']
         attrs = {'class': 'tableStyle'}
+
+
+class VendorTable(tables.Table):
+
+    edit_link = tables.LinkColumn('vendor', args=[A('pk')],
+                                  verbose_name='Edit Vendor', text='Edit', accessor='pk',
+                                  attrs={'class': 'edit_link'})
+
+    delete_link = tables.LinkColumn('vendor_delete', args=[A('pk')],
+                                    verbose_name='Delete Vendor', text='Delete', accessor='pk',
+                                    attrs={'class': 'delete_link'})
+
+    class Meta:
+        model = Party
+        attrs = {'class': 'tableStyle'}
+
+
+class VendorAddress(tables.Table):
+
+    edit_link = tables.LinkColumn('edit_address', args=[A('id')],
+                                  verbose_name='Edit Address', text='Edit', accessor='id',
+                                  attrs={'class': 'edit_link'})
+
+    delete_link = tables.LinkColumn('delete_address', args=[A('id')],
+                                    verbose_name='Delete Address', text='Delete', accessor='id',
+                                    attrs={'class': 'delete_link'})
+
+
+    class Meta:
+        model = PartyAddress
+        fields = ['address_id.street', 'address_id.suburb', 'address_id.state', 'address_id.postcode']
+        attrs = {'class': 'tableStyle'}
--- party/urls.py
+++ party/urls.py
@@ -8,7 +8,18 @@ urlpatterns = [
     url(r'^customers/new/$', views.customer_new, name='customer_new'),
     url(r'^customers/(?P<account_number>\w+)/$', views.customer, name='customer'),
     url(r'^customers/delete/(?P<account_number>\w+)/$', views.customer_delete, name='customer_delete'),
-    url(r'^customers/address/(?P<id>\w+)/$', views.edit_address, name='edit_address'),
-    url(r'^customers/address/delete/(?P<id>\w+)/$', views.delete_address, name='delete_address'),
+
+
+    # Vendor URL patterns
+    url(r'^vendors/$', views.vendors, name='vendors'),
+    url(r'^vendors/all_vendors/$', views.all_vendors, name='all_vendors'),
+    url(r'^vendors/new/$', views.vendor_new, name='vendor_new'),
+    url(r'^vendors/(?P<account_number>\w+)/$', views.vendor, name='vendor'),
+    url(r'^vendors/delete/(?P<account_number>\w+)/$', views.vendor_delete, name='vendor_delete'),
+
+
+    # Party address URL patterns
+    url(r'^address/(?P<id>\w+)/$', views.edit_address, name='edit_address'),
+    url(r'^address/delete/(?P<id>\w+)/$', views.delete_address, name='delete_address'),
 
 ]
